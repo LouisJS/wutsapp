e6dfac0f1090284ca20421f40c55be014e5b7747
Louis Lecocq <my.lecocq@gmail.com>
2018-01-15 17:48:04 +0100

    fix: Add some mocked data

--- helpers/mockData.js
+++ helpers/mockData.js
@@ -13,6 +13,7 @@ export const data = {
     {"id": 'user2', "phone": "0634234532", "name": "Julien",  "profile": "https://res.cloudinary.com/donglyhya/image/upload/v1515857705/user3_tgy9bd.jpg"},
     {"id": 'user3', "phone": "0623423423", "name": "Sophia",  "profile": "https://res.cloudinary.com/donglyhya/image/upload/v1515857705/user5_f7ls8m.jpg"},
     {"id": 'user4', "phone": "0787345873", "name": "Matt",    "profile": "https://res.cloudinary.com/donglyhya/image/upload/v1515857705/user4_g6lmmh.jpg"},
+    {"id": 'user5', "phone": "0943534534", "name": "Pam",     "profile": "https://res.cloudinary.com/donglyhya/image/upload/v1515857706/user1_jwft1x.jpg"},
   ],
   "chats": {
     'user2': [
@@ -41,7 +42,7 @@ export const data = {
       {
         'sender': 'user1',
         'date': '2018-01-13T14:31:54+00:00',
-        'content': `Hey Dude`
+        'content': `Hey There !`
       },
       {
         'sender': 'user1',
@@ -72,6 +73,70 @@ export const data = {
         'sender': 'user3',
         'date': '2018-01-14T16:25:23+00:00',
         'content': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation`
+      },
+    ],
+    'user4': [
+      {
+        'sender': 'user4',
+        'date': '2018-01-13T14:33:23+00:00',
+        'content': `Hi Man !`
+      },
+      {
+        'sender': 'user4',
+        'date': '2018-01-14T14:25:23+00:00',
+        'content': `Wanna play some games ?`
+      },
+      {
+        'sender': 'user1',
+        'date': '2018-01-13T14:31:54+00:00',
+        'content': `Hey There !`
+      },
+      {
+        'sender': 'user1',
+        'date': '2018-01-13T14:32:54+00:00',
+        'content': `Sorry man, i have to make a project for a cool startup`
+      },
+      {
+        'sender': 'user4',
+        'date': '2018-01-13T14:32:54+00:00',
+        'content': `Oh Okay ! Let me know how it goes !`
+      },
+      {
+        'sender': 'user1',
+        'date': '2018-01-13T14:32:54+00:00',
+        'content': `Sure. I will ;)`
+      },
+    ],
+    'user5': [
+      {
+        'sender': 'user5',
+        'date': '2018-01-14T14:25:23+00:00',
+        'content': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation`
+      },
+      {
+        'sender': 'user5',
+        'date': '2018-01-14T15:25:23+00:00',
+        'content': `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation`
+      },
+      {
+        'sender': 'user5',
+        'date': '2018-01-14T16:25:23+00:00',
+        'content': `Lorem ipsum dolor sit amet, magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation`
+      },
+      {
+        'sender': 'user1',
+        'date': '2018-01-13T14:31:54+00:00',
+        'content': `Hello Captain o/`
+      },
+      {
+        'sender': 'user1',
+        'date': '2018-01-13T14:31:59+00:00',
+        'content': `Have you seen The Office ? This tv show is awesome`
+      },
+      {
+        'sender': 'user1',
+        'date': '2018-01-13T14:32:54+00:00',
+        'content': `I love it so much`
       },
     ],
   },
